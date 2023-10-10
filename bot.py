61ab7d2b855a4fade0346d819825778397142e33
Svennerin <60832064+Svennerin@users.noreply.github.com>
2023-10-10 11:51:46 -0400

    secondmain update
    secondmain does run flask via command line.

--- bot.py
+++ bot.py
@@ -1,11 +1,24 @@
 import slack_sdk
 import os
 from pathlib import Path
-from dotenv.main import load_dotenv
+from dotenv import load_dotenv
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
 
-env_path = Path('.')/ '.env'
+env_path = Path('.') / '.env'
+
 load_dotenv(dotenv_path=env_path)
+
+app = Flask(__name__)
+
+
+
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events',app)
 
 client = slack_sdk.WebClient(token = os.environ['SLACK_TOKEN'])
 
 client.chat_postMessage(channel='#bot', text="This is a botspace.")
+
+
+if __name__ == "__main__":
+	app.run(debug=True, port=5001)
